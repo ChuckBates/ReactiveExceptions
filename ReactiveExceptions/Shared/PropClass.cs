61d0097e63d60e4260a724fdc7589670d568b397
SovietBear <Numskull2@3#>
2018-10-30 00:59:26 -0600

    add

---
+++ ReactiveExceptions/Shared/PropClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactiveExceptions.Shared
+{
+    public class PropClass
+    {
+        public string Name { get; set; }
+
+        public PropClass(string name)
+        {
+            Name = name;
+        }
+    }
+}
